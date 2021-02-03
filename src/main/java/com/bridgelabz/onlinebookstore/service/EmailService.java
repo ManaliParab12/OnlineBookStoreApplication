9605a1124dd2bb66af2cde3517075be50282713c
ManaliParab12 <manaliparab10@gmail.com>
2021-02-03 19:59:07 +0530

    Defined methods in Email service class to send verification mail while registration

--- src/main/java/com/bridgelabz/onlinebookstore/service/EmailService.java
+++ src/main/java/com/bridgelabz/onlinebookstore/service/EmailService.java
@@ -3,19 +3,29 @@ package com.bridgelabz.onlinebookstore.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
-import com.bridgelabz.onlinebookstore.dto.EmailDTO;
+import com.bridgelabz.onlinebookstore.dto.ResponseDTO;
+import com.bridgelabz.onlinebookstore.model.User;
+import com.bridgelabz.onlinebookstore.utility.Token;
 
-public class EmailService {
+@Service
+public class EmailService implements IEmailService {
 	
 	@Autowired
-	private JavaMailSender mailSender;	
+	JavaMailSender mailSender;	
 	
-	public void sendRegistrationMail(EmailDTO emailDTO) {
+	public void sendRegistrationMail(User user) {
 		SimpleMailMessage mailMessage = new SimpleMailMessage();
-		mailMessage.setTo(emailDTO.getEmail());
-		mailMessage.setSubject(emailDTO.getSubject());
-		mailMessage.setText(emailDTO.getMessage());
+		mailMessage.setTo(user.getEmail());
+		mailMessage.setSubject("Verification Link");
+		mailMessage.setText("Thank you for registration");
 		mailSender.send(mailMessage);		
+	}
+
+	public ResponseDTO verificationMail(User user) {
+		String token = Token.generateToken(user.getId());
+		this.sendRegistrationMail(user);
+		return ResponseDTO.getResponse("verification mail sent",token);
 	}
 }
