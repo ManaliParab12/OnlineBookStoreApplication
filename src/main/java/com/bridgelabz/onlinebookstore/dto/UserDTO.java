06676c4c97fa1cb2c0c051090f62233c7fe45d18
ManaliParab12 <manaliparab10@gmail.com>
2021-02-04 19:39:43 +0530

    Added regex pattern validation on email and password

--- src/main/java/com/bridgelabz/onlinebookstore/dto/UserDTO.java
+++ src/main/java/com/bridgelabz/onlinebookstore/dto/UserDTO.java
@@ -12,15 +12,15 @@ import lombok.ToString;
 public @ToString @Data class UserDTO {
 	
 	@NotEmpty(message = "First Name cannot be Empty")
-	@Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "User name Invalid")
+	@Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "First name is  Invalid")
 	public String firstName;
 	 
 	@NotEmpty(message = "Last Name cannot be Empty")
-	@Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "User name Invalid")
+	@Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Last name is Invalid")
 	public String lastName;
 	
 	@NotEmpty(message = "Email cannot be Empty")
-	@Email(message = "Enter a valid email address")
+	@Pattern(regexp = "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", message = "Enter a valid email address")
 	public String email;
 	
 	@Pattern(regexp="(^$|[0-9]{10})", message = "Mobile number must be 10")
@@ -29,6 +29,7 @@ public @ToString @Data class UserDTO {
 	
 	@NotEmpty(message = "Enter your password")
 	@Length(min = 8, message = "Password must be at least 8 characters")
+	@Pattern(regexp = "(?=.*[A-Z])(?=.*[0-9])(?=.*[!?#@$]{1})[a-zA-Z0-9!?#@$]{8,32}$", message = "Password invalid")
 	public String password;
 	
 	public String type;
