aada8756e5abd80abb6162bd63aeee384c7d5370
ManaliParab12 <manaliparab10@gmail.com>
2021-02-05 21:14:06 +0530

    Defined method to read book data from csv file

--- src/main/java/com/bridgelabz/onlinebookstore/service/BookService.java
+++ src/main/java/com/bridgelabz/onlinebookstore/service/BookService.java
@@ -1,5 +1,10 @@
 package com.bridgelabz.onlinebookstore.service;
 
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +18,8 @@ import com.bridgelabz.onlinebookstore.model.User;
 import com.bridgelabz.onlinebookstore.repository.BookRepository;
 import com.bridgelabz.onlinebookstore.repository.UserRepository;
 import com.bridgelabz.onlinebookstore.utility.Token;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
 
 @Service
 public class BookService implements IBookService {
@@ -43,5 +50,25 @@ public class BookService implements IBookService {
 		List<Book> books = bookRepository.findAll();
 		return books;
 	}
+	
+	private List<Book> getBookFromCsv() {
+		try (Reader reader = Files.newBufferedReader(Paths.get("./src/main/resources/books_data.csv"));){
+				CsvToBeanBuilder<Book> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+	            csvToBeanBuilder.withType(Book.class);
+	            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+	            CsvToBean<Book> csvToBean = csvToBeanBuilder.build();
+	            Iterator<Book> bookCSVIterator = csvToBean.iterator();
+	            List <Book> bookList = new ArrayList<>();
+				 while (bookCSVIterator.hasNext()) {
+		                Book bookData = bookCSVIterator.next();
+		                bookList.add(bookData);
+		            }
+		            System.out.println(bookList);
+		            return bookList;
+		}catch(Exception e) {
+		System.out.println("Exception while reading csv" + e);
+	}
+	return null;
+	}	
 
 }
