e781cd5118321b9796365c5b4fb0e33ddc03f668
ManaliParab12 <manaliparab10@gmail.com>
2021-02-09 19:33:32 +0530

    Used Rabbitmq to send verification and reset-password link mails to user

--- src/main/java/com/bridgelabz/onlinebookstore/service/EmailService.java
+++ src/main/java/com/bridgelabz/onlinebookstore/service/EmailService.java
@@ -1,5 +1,9 @@
 package com.bridgelabz.onlinebookstore.service;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -9,15 +13,30 @@ import com.bridgelabz.onlinebookstore.dto.EmailDTO;
 import com.bridgelabz.onlinebookstore.dto.ResponseDTO;
 import com.bridgelabz.onlinebookstore.model.User;
 import com.bridgelabz.onlinebookstore.utility.Token;
+import com.google.gson.Gson;
 
 @Service
 public class EmailService implements IEmailService {
 	
 	@Autowired
-	JavaMailSender mailSender;	
+	JavaMailSender mailSender;
 	
-	public void sendMail(EmailDTO emailDTO) {
+	@Autowired
+	Gson gson;
+	
+	@Autowired
+	Queue queue;
+	
+	 @Autowired
+	 private RabbitTemplate rabbitTemplate;
+	 
+	 @Autowired
+	 private Binding bind;
+	
+	@RabbitListener(queues = "${rabbitmq.queue}")
+	public void sendMail(String email) {
 		SimpleMailMessage mailMessage = new SimpleMailMessage();
+		EmailDTO emailDTO = gson.fromJson(email, EmailDTO.class);
 		mailMessage.setTo(emailDTO.getEmail());
 		mailMessage.setSubject(emailDTO.getSubject());
 		mailMessage.setText(emailDTO.getMessage());
@@ -27,7 +46,9 @@ public class EmailService implements IEmailService {
 	@Override
 	public ResponseDTO sendVerificationMail(User user) {
 		String token = Token.generateToken(user.getId());
-		this.sendMail(new EmailDTO(user.getEmail(), "verification Link ", verificationURL(token)));
+		rabbitTemplate.convertAndSend(bind.getExchange(), bind.getRoutingKey(),
+					   gson.toJson(new EmailDTO(user.getEmail(), "verification Link ", verificationURL(token))));
+//		this.sendMail(new EmailDTO(user.getEmail(), "verification Link ", verificationURL(token)));
 		return new ResponseDTO("verification link has been sent to your registered email id : " +user.getEmail());
 	}
 	
@@ -41,7 +62,9 @@ public class EmailService implements IEmailService {
 	@Override
 	public ResponseDTO sendResetPasswordMail(User user) {
 		String token = Token.generateToken(user.getId());
-		this.sendMail(new EmailDTO(user.getEmail(), "Reset Password Link", resetURL(token)));
+		rabbitTemplate.convertAndSend(bind.getExchange(), bind.getRoutingKey(),
+					   gson.toJson(new EmailDTO(user.getEmail(), "Reset Password Link ",  resetURL(token))));
+//		this.sendMail(new EmailDTO(user.getEmail(), "Reset Password Link", resetURL(token)));
 		return new ResponseDTO("Reset Password link has been sent to your registered email id : " +user.getEmail());
 	}
 	
